ccb3f60d82c673840980f89295b2a93f17c902d3
Nagarjuna4085 <nagarjuna.sanem@fmail.com>
2023-09-08 02:18:14 +0530

    project initiated and added the vue browser extension

---
+++ src/popup/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>Nagarjuna</div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+html {
+  width: 400px;
+  height: 400px;
+  color: red;
+}
+</style>
